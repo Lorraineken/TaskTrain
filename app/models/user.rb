5b0174e69798c1d6e54f233961f9abe6462bc063
Lorraineken <lorrainekupa@gmail.com>
2023-03-22 10:53:40 +0300

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    validates :username, {
+        length: {minimum:5, maximum:8}, 
+        uniqueness: true,
+        presence: true
+    }
+    validates :email, {
+        presence: true,
+        uniqueness: true
+    }
+end
