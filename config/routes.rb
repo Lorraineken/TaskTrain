1d88caefc481338ad0c96b31406da5a5761a4dfa
Lorraineken <lorrainekupa@gmail.com>
2023-03-26 10:41:59 +0300

    Added todos resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   #user
   post '/users', to:'users#register'
-  
+  post '/users/login', to:'users#login'
+  get '/users/login/check', to:'users#check_login_status'
+  delete'/users/logout', to: 'users#logout'
+
+  post'/todos', to: 'todos#create'
+  put'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get'/todos', to: 'todos#index'
 end
